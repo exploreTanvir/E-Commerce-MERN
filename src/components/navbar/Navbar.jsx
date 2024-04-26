f66fecc1623681ef0622ebab867e6bf3eeb3ee20
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-04-26 15:01:38 +0600

    MERN E-Commerce

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="container">
+        <div className="wrapper">
+            <div className="left">left</div>
+            <div className="center">center</div>
+            <div className="right">right</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
