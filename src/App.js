5b450d2e064263beffb7865dd51ecb4e83cdc250
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-04-27 16:45:46 +0600

    MERN E-Commerce

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
-    <div>
+    <div >
       <Navbar/>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,10 +4,19 @@ import "./navbar.css"
 const Navbar = () => {
   return (
     <div className="container">
-        <div className="wrapper">
-            <div className="left">left</div>
-            <div className="center">center</div>
-            <div className="right">right</div>
+        <div className="wrapper align-items-center justify-content-between d-flex">
+            <div className="left col-4 d-flex align-items-center">
+              <div className="language fs-5">EN
+              </div>
+              <div className="searchContainer p-1 ms-2 d-flex align-items-center border border-2">
+                <input type="text" className='border-0' />
+                <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+            <div className="center col-4">
+              center
+            </div>
+            <div className="right col-4">right</div>
         </div>
     </div>
   )
