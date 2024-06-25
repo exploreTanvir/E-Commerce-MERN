b383df2c89d68d99c4b447134c2575b59ee2d890
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-06-25 20:40:57 +0600

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
             <input type="text" placeholder="Password"/>
             
         <button className="border-0 text-white mb-3 rounded-2 pointer-event">LOGIN</button>
-        <a href="/">DO YOU NOT REMEMBER THE PARSSWORD?</a>
+        <a href="/">DO YOU NOT REMEMBER THE PARSSWORD? </a>
         <a href="/">CREATE A NEW ACCOUNT.</a>
         </form>
     </div>
